d94c1db778db9356d0fab5014773234f089c3c88
AnhTT <tuananh.k58@gmail.com>
2026-05-30 22:56:45 +0700

    feat: update metadata icons in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ export const metadata: Metadata = {
   title: "Sổ tay gia tiên",
   description:
     "Gìn giữ cội nguồn, kết nối tương lai với ứng dụng lập gia phả và quản lý ngày giỗ cho gia đình Việt.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
